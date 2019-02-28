1770d5eab3ea455e3b7d25d7c31d381efcb93dfe
Jeremy <jeremy.koskamp@live.nl>
2019-02-28 22:04:00 +0100

    sum

---
+++ HoboDogo/Assets/Scripts/Boid_Master.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid_Master : MonoBehaviour {
+
+    private List<GameObject> boids;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Boid"))
+        {
+            boids.Add(other.gameObject);
+        }
+    }
+}
